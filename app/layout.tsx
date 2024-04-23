999f46c33228a710b554a8116d0ad1f0b95d08ab
Bridger Tower <bridgertower@gmail.com>
2024-04-23 08:13:13 -0600

    add navigation menu

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,18 @@
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
-import "./globals.css";
 import { ThemeProvider } from "@/components/theme/theme-provider";
+
+import "./globals.css";
+
+import { Button } from "@/components/ui/button";
+import { NavMenu } from "@/components/nav/nav-menu";
+import { MobileNav } from "@/components/nav/mobile-nav";
+import { ThemeToggle } from "@/components/theme/theme-toggle";
+
+import Logo from "@/public/logo.svg";
+
+import Image from "next/image";
+import Link from "next/link";
 
 import { cn } from "@/lib/utils";
 
@@ -32,9 +43,44 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <Nav />
           {children}
         </ThemeProvider>
       </body>
     </html>
   );
 }
+
+const Nav = ({ className, children, id }: NavProps) => {
+  return (
+    <nav
+      className={cn("sticky z-50 top-0 bg-background", "border-b", className)}
+      id={id}
+    >
+      <div
+        id="nav-container"
+        className="max-w-5xl mx-auto py-4 px-6 sm:px-8 flex justify-between items-center"
+      >
+        <Link className="hover:opacity-75 transition-all" href="/">
+          <h2 className="sr-only">Craft UI</h2>
+          <Image
+            src={Logo}
+            alt="Logo"
+            className="dark:invert"
+            width={72}
+            height={48}
+          ></Image>
+        </Link>
+        {children}
+        <div className="flex items-center gap-2">
+          <NavMenu />
+          <Button asChild>
+            <Link href="https://9d8.dev">Get Started</Link>
+          </Button>
+          <ThemeToggle />
+          <MobileNav />
+        </div>
+      </div>
+    </nav>
+  );
+};
